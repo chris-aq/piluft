d2df61f5dfde91a3c27d9adb46e3bcfe831e2baf
chris-aq <48091480+chris-aq@users.noreply.github.com>
2024-01-02 11:24:12 +0000

    Update logaq.py for python 3

--- src/logaq.py
+++ src/logaq.py
@@ -5,7 +5,7 @@
 # Notes:
 # To allow user to access the tty port: sudo usermod -a -G dialout <user>
 
-from __future__ import print_function
+
 import serial, struct, sys, time, datetime, os
 import csv
 import numpy as np
@@ -65,7 +65,7 @@ class LuftDaten() :
                 req = requests.post(url,
                     json={
                         "software_version": "python-lufty 0.1",
-                        "sensordatavalues": [{"value_type": key, "value": val} for key, val in values.items()],
+                        "sensordatavalues": [{"value_type": key, "value": val} for key, val in list(values.items())],
                     },
                     headers={
                         "X-PIN":    str(pin),
@@ -79,60 +79,169 @@ class LuftDaten() :
                 syslog.syslog(syslog.LOG_DEBUG, "Exception pushing data to Luftdaten: " + str(e))
                 time.sleep(2)
 
+class SDS011(object):
+    """Provides method to read from a SDS011 air particlate density sensor
+    using UART.
+    """
 
-""" Class to control and take readings from the SDS011 air quality sensor.
-"""
-class SDS011() :
+    HEAD = b'\xaa'
+    TAIL = b'\xab'
+    CMD_ID = b'\xb4'
 
-    def __init__(self, serial_port="/dev/ttyUSB0"):
-        self.ser = serial.Serial(timeout=8, write_timeout=8)
-        self.ser.port = serial_port
-        self.ser.baudrate = 9600
+    # The sent command is a read or a write
+    READ = b"\x00"
+    WRITE = b"\x01"
 
-        self.ser.open()
-        self.ser.flushInput()
+    REPORT_MODE_CMD = b"\x02"
+    ACTIVE = b"\x00"
+    PASSIVE = b"\x01"
 
+    QUERY_CMD = b"\x04"
 
-    def dump(self, d, prefix=''):
-        print(prefix + ' '.join(x.encode('hex') for x in d))
+    # The sleep command ID
+    SLEEP_CMD = b"\x06"
+    # Sleep and work byte
+    SLEEP = b"\x00"
+    WORK = b"\x01"
 
-    def construct_command(self, cmd, data=[]):
-        assert len(data) <= 12
-        data += [0,]*(12-len(data))
-        checksum = (sum(data)+cmd-2)%256
-        ret = "\xaa\xb4" + chr(cmd)
-        ret += ''.join(chr(x) for x in data)
-        ret += "\xff\xff" + chr(checksum) + "\xab"
+    # The work period command ID
+    WORK_PERIOD_CMD = b'\x08'
 
-        if DEBUG:
-            dump(ret, '> ')
-        return ret
+    def __init__(self, serial_port, baudrate=9600, timeout=2,
+                 use_query_mode=True):
+        """Initialise and open serial port.
+        """
+        self.ser = serial.Serial(port=serial_port,
+                                 baudrate=baudrate,
+                                 timeout=timeout)
+        self.ser.flush()
+        self.set_report_mode(active=not use_query_mode)
 
-    def process_data(self, d):
-        r = struct.unpack('<HHxxBB', d[2:])
-        pm25 = r[0]/10.0
-        pm10 = r[1]/10.0
-        checksum = sum(ord(v) for v in d[2:8])%256
-        return [pm25, pm10]
-        syslog.syslog(syslog.LOG_DEBUG, "PM 2.5: {} μg/m^3  PM 10: {} μg/m^3 CRC={}".format(pm25, pm10, "OK" if (checksum==r[2] and r[3]==0xab) else "NOK"))
-        #print("PM 2.5: {} μg/m^3  PM 10: {} μg/m^3 CRC={}".format(pm25, pm10, "OK" if (checksum==r[2] and r[3]==0xab) else "NOK"))
+    def _execute(self, cmd_bytes):
+        """Writes a byte sequence to the serial.
+        """
+        self.ser.write(cmd_bytes)
 
-    def process_version(self, d):
-        r = struct.unpack('<BBBHBB', d[3:])
-        checksum = sum(ord(v) for v in d[2:8])%256
-        print("Y: {}, M: {}, D: {}, ID: {}, CRC={}".format(r[0], r[1], r[2], hex(r[3]), "OK" if (checksum==r[4] and r[5]==0xab) else "NOK"))
+    def _get_reply(self):
+        """Read reply from device."""
+        raw = self.ser.read(size=10)
+        data = raw[2:8]
+        if len(data) == 0:
+            return None
+        if (sum(d for d in data) & 255) != raw[8]:
+            return None  #TODO: also check cmd id
+        return raw
 
-    def read_response(self):
+    def cmd_begin(self):
+        """Get command header and command ID bytes.
+        @rtype: list
+        """
+        return self.HEAD + self.CMD_ID
+
+    def set_report_mode(self, read=False, active=False):
+        """Get sleep command. Does not contain checksum and tail.
+        @rtype: list
+        """
+        cmd = self.cmd_begin()
+        cmd += (self.REPORT_MODE_CMD
+                + (self.READ if read else self.WRITE)
+                + (self.ACTIVE if active else self.PASSIVE)
+                + b"\x00" * 10)
+        cmd = self._finish_cmd(cmd)
+        self._execute(cmd)
+        self._get_reply()
+
+    def query(self):
+        """Query the device and read the data.
+
+        @return: Air particulate density in micrograms per cubic meter.
+        @rtype: tuple(float, float) -> (PM2.5, PM10)
+        """
+        cmd = self.cmd_begin()
+        cmd += (self.QUERY_CMD
+                + b"\x00" * 12)
+        cmd = self._finish_cmd(cmd)
+        self._execute(cmd)
+
+        raw = self._get_reply()
+        if raw is None:
+            return None  #TODO:
+        data = struct.unpack('<HH', raw[2:6])
+        pm25 = data[0] / 10.0
+        pm10 = data[1] / 10.0
+        return (pm25, pm10)
+
+    def sleep(self, read=False, sleep=True):
+        """Sleep/Wake up the sensor.
+
+        @param sleep: Whether the device should sleep or work.
+        @type sleep: bool
+        """
+        cmd = self.cmd_begin()
+        cmd += (self.SLEEP_CMD
+                + (self.READ if read else self.WRITE)
+                + (self.SLEEP if sleep else self.WORK)
+                + b"\x00" * 10)
+        cmd = self._finish_cmd(cmd)
+        self._execute(cmd)
+        self._get_reply()
+
+    def set_work_period(self, read=False, work_time=0):
+        """Get work period command. Does not contain checksum and tail.
+        @rtype: list
+        """
+        assert work_time >= 0 and work_time <= 30
+        cmd = self.cmd_begin()
+        cmd += (self.WORK_PERIOD_CMD
+                + (self.READ if read else self.WRITE)
+                + bytes([work_time])
+                + b"\x00" * 10)
+        cmd = self._finish_cmd(cmd)
+        self._execute(cmd)
+        self._get_reply()
+
+    def _finish_cmd(self, cmd, id1=b"\xff", id2=b"\xff"):
+        """Add device ID, checksum and tail bytes.
+        @rtype: list
+        """
+        cmd += id1 + id2
+        checksum = sum(d for d in cmd[2:]) % 256
+        cmd += bytes([checksum]) + self.TAIL
+        return cmd
+
+    def _process_frame(self, data):
+        """Process a SDS011 data frame.
+
+        Byte positions:
+            0 - Header
+            1 - Command No.
+            2,3 - PM2.5 low/high byte
+            4,5 - PM10 low/high
+            6,7 - ID bytes
+            8 - Checksum - sum of bytes 2-7
+            9 - Tail
+        """
+        raw = struct.unpack('<HHxxBBB', data[2:])
+        checksum = sum(v for v in data[2:8]) % 256
+        if checksum != data[8]:
+            return None
+        pm25 = raw[0] / 10.0
+        pm10 = raw[1] / 10.0
+        return (pm25, pm10)
+
+    def read(self):
+        """Read sensor data.
+
+        @return: PM2.5 and PM10 concetration in micrograms per cude meter.
+        @rtype: tuple(float, float) - first is PM2.5.
+        """
         byte = 0
-        while byte != "\xaa":
+        while byte != self.HEAD:
             byte = self.ser.read(size=1)
-            if len(byte) == 0 : return None
-
-        d = self.ser.read(size=9)
-
-        if DEBUG:
-            dump(d, '< ')
-        return byte + d
+            d = self.ser.read(size=10)
+            if d[0:1] == b"\xc0":
+                data = self._process_frame(byte + d)
+                return data
 
     def reset_serial(self) :
         self.ser.reset_input_buffer()
@@ -140,98 +249,10 @@ class SDS011() :
         self.ser.close()
         self.ser.open()
 
-    def cmd_set_mode(self, mode=MODE_QUERY):
-        self.ser.write(self.construct_command(CMD_MODE, [0x1, mode]))
-        self.read_response()
+            
 
-    def cmd_query_data(self):
-        syslog.syslog(syslog.LOG_DEBUG, "Sending CMD_QUERY_DATA")
-        self.ser.write(self.construct_command(CMD_QUERY_DATA))
-        syslog.syslog(syslog.LOG_DEBUG, "Waiting for response")
-        d = self.read_response()
-        if d is None : return None
-        syslog.syslog(syslog.LOG_DEBUG, "Response received")
-        values = []
-        if d[1] == "\xc0":
-            values = self.process_data(d)
-        return values
-
-    def cmd_set_sleep(self, sleep=1):
-        mode = 0 if sleep else 1
-        self.ser.write(self.construct_command(CMD_SLEEP, [0x1, mode]))
-        self.read_response()
-
-    def cmd_set_working_period(self, period):
-        self.ser.write(self.construct_command(CMD_WORKING_PERIOD, [0x1, period]))
-        self.read_response()
-
-    def cmd_firmware_ver(self):
-        self.ser.write(self.construct_command(CMD_FIRMWARE))
-        d = self.read_response()
-        process_version(d)
-
-    def cmd_set_id(self, id):
-        id_h = (id>>8) % 256
-        id_l = id % 256
-        self.ser.write(self.construct_command(CMD_DEVICE_ID, [0]*10+[id_l, id_h]))
-        self.read_response()
-
-    # Set the SDS011 measurement logger running
-    def run(self) :
-
-        luft = LuftDaten(sensorID=lufdaten_id)
-
-        while True:
-            self.cmd_set_sleep(0)
-            self.cmd_set_mode(1);
-            time.sleep(2)
-
-            # Values lists for pm value median calculation
-            values_pm25 = []
-            values_pm10 = []
-
-            for t in range(NUM_READINGS):
-                time.sleep(3)
-                values = self.cmd_query_data();
-                if values is not None and len(values) == 2 :
-                    values_pm25.append(values[0])
-                    values_pm10.append(values[1])
-                    syslog.syslog(syslog.LOG_DEBUG, "PM2.5: " + str(values[0]) + ", PM10: " + str(values[1]))
-                    print("PM2.5: ", values[0], ", PM10: ", values[1])
-                else:
-                    syslog.syslog(syslog.LOG_DEBUG, "Reading failed - resetting serial device")
-                    print("Reading failed - resetting serial device")
-                    time.sleep(3)
-                    self.reset_serial()
-                    break
-
-
-            # Set the SDS011 to sleep
-            self.cmd_set_mode(0);
-            self.cmd_set_sleep()
-            syslog.syslog(syslog.LOG_DEBUG, "SDS011 sensor set to sleep")
-
-            # Log the values and send to Luftdaten
-            if len(values_pm25) == NUM_READINGS and len(values_pm10) == NUM_READINGS :
-                median_pm25 = np.median(values_pm25)
-                median_pm10 = np.median(values_pm10)
-
-                # Log the data to csv file
-                with open(AIR_QUALITY_FILENAME, "a+") as f:
-                    writer = csv.writer(f, delimiter=",")
-                    writer.writerow([datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"), round(median_pm25, 2), round(median_pm10, 2)])
-                    f.flush()
-
-                # Send the data to Luftdaten - PM10 and PM25
-                luft.send(median_pm10, median_pm25)
-
-
-            # Sleep until the next measurement interval has expired
-            sleep_period = (PERIOD - time.time()) % PERIOD
-            syslog.syslog(syslog.LOG_DEBUG, "Going to sleep for " + str(sleep_period) + "s")
-            print ("Going to sleep for", sleep_period, "s")
-            time.sleep(sleep_period)
-            print ("Waking up")
+""" Class to control and take readings from the SDS011 air quality sensor.
+"""
 
 
 # Main program
@@ -247,5 +268,59 @@ if __name__ == "__main__":
     print("Using serial device: " + tty_device)
 
     sds011 = SDS011(serial_port=tty_device)
-    sds011.run()
+    sds011 = SDS011("/dev/ttyUSB0", use_query_mode=True)
+
+
+    luft = LuftDaten(sensorID=lufdaten_id)
+
+    while True:
+        sds011.sleep(sleep=False)
+        # self.cmd_set_mode(1);
+        time.sleep(2)
+
+        # Values lists for pm value median calculation
+        values_pm25 = []
+        values_pm10 = []
+
+        for t in range(NUM_READINGS):
+            time.sleep(3)
+            values = sds011.query()
+            if values is not None and len(values) == 2 :
+                values_pm25.append(values[0])
+                values_pm10.append(values[1])
+                syslog.syslog(syslog.LOG_DEBUG, "PM2.5: " + str(values[0]) + ", PM10: " + str(values[1]))
+                print("PM2.5: ", values[0], ", PM10: ", values[1])
+            else:
+                syslog.syslog(syslog.LOG_DEBUG, "Reading failed - resetting serial device")
+                print("Reading failed - resetting serial device")
+                time.sleep(3)
+                sds011.reset_serial()
+                break
+
+
+        # Set the SDS011 to sleep
+        sds011.sleep()
+        syslog.syslog(syslog.LOG_DEBUG, "SDS011 sensor set to sleep")
+
+        # Log the values and send to Luftdaten
+        if len(values_pm25) == NUM_READINGS and len(values_pm10) == NUM_READINGS :
+            median_pm25 = np.median(values_pm25)
+            median_pm10 = np.median(values_pm10)
+
+            # Log the data to csv file
+            with open(AIR_QUALITY_FILENAME, "a+") as f:
+                writer = csv.writer(f, delimiter=",")
+                writer.writerow([datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"), round(median_pm25, 2), round(median_pm10, 2)])
+                f.flush()
+
+            # Send the data to Luftdaten - PM10 and PM25
+            luft.send(median_pm10, median_pm25)
+
+
+        # Sleep until the next measurement interval has expired
+        sleep_period = (PERIOD - time.time()) % PERIOD
+        syslog.syslog(syslog.LOG_DEBUG, "Going to sleep for " + str(sleep_period) + "s")
+        print ("Going to sleep for", sleep_period, "s")
+        time.sleep(sleep_period)
+        print ("Waking up")
 
